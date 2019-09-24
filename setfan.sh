3bed6063a83a4529cbc5cb0bc2083a1be959aaa4
czombos <czombostamas@gmail.com>
2019-09-24 16:45:11 +0200

    Create setfan.sh

---
+++ setfan.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+  
+cards=$(find /sys/class/drm -regextype posix-awk -regex ".*/card[0-9]{1,2}")
+
+for i in $cards
+do
+  managefilepath="$i/device/power_dpm_force_performance_level"
+  fanfilepath=$(find "$i"/device/hwmon -regextype posix-awk -regex ".*/hwmon[0-9]{1,2}")/pwm1_enable
+  catfan=$(cat "$fanfilepath")
+  echo "$fanfilepath set $catfan" 
+  sudo /bin/su -c "echo 1 > $fanfilepath"
+  echo "$managefilepath set manual" 
+  sudo /bin/su -c "echo manual > $managefilepath"
+done
